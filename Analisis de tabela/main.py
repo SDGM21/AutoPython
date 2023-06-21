50d395b32b7447f1cc3ddceec64785779cce07c0
Santiago David <santiagodaviduno@hotmail.com>
2023-06-21 01:03:30 -0300

    keep coding

--- Analisis de tabela/main.py
+++ Analisis de tabela/main.py
@@ -1,3 +1,9 @@
+from get_tabela_data import get_tabela
+import plotly.express as px
 if __name__ == '__main__':
-    
+    # for cols in get_tabela().columns:
+        
+    grafico = px.histogram(get_tabela(), x=get_tabela().columns[2], y="Nota (1-100)", text_auto=True, histfunc='avg', nbins=10)
+        # pass
+    grafico.show()
     pass

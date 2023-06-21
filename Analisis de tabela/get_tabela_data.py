49652829e8c3cbaeccb6fb832ad4b96d5540d79e
Santiago David <santiagodaviduno@hotmail.com>
2023-06-21 00:13:07 -0300

    Update get_tabela_data.py

--- Analisis de tabela/get_tabela_data.py
+++ Analisis de tabela/get_tabela_data.py
@@ -1,6 +1,11 @@
 import pathlib
 import pandas as pd
 
-tabela = pd.read_csv(pathlib.Path("Analisis de tabela/clientes.csv"), encoding="latin1", sep=';')
-tabela = tabela.drop(columns=["Unnamed: 8", "Idade"])
-print(tabela)
+def get_tabela():
+    tabela = pd.read_csv(pathlib.Path("Analisis de tabela/clientes.csv"), encoding="latin1", sep=';')
+    tabela = tabela.dropna()
+    tabela = tabela.drop(columns=["Unnamed: 8"])
+    tabela["Salário Anual (R$)"] = pd.to_numeric(tabela["Salário Anual (R$)"], 'coerce')
+
+    return tabela
+

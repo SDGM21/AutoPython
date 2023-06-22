bcca614a9545945670a033c445f41422df6c1f7d
Santiago David <santiagodaviduno@hotmail.com>
2023-06-22 19:22:04 -0300

    Ended

--- Analisis de tabela/get_tabela_data.py
+++ Analisis de tabela/get_tabela_data.py
@@ -1,11 +1,13 @@
 import pathlib
+from kiwisolver import UnknownConstraint
 import pandas as pd
 
+
 def get_tabela():
-    tabela = pd.read_csv(pathlib.Path("Analisis de tabela/clientes.csv"), encoding="latin1", sep=';')
-    # tabela = tabela.dropna()
+    tabela = pd.read_csv(
+        pathlib.Path("Analisis de tabela/clientes.csv"), encoding="latin1", sep=";"  # type: ignore
+    )
     tabela = tabela.drop(columns=["Unnamed: 8"])
-    tabela["Salário Anual (R$)"] = pd.to_numeric(tabela["Salário Anual (R$)"], 'coerce')
-
+    tabela = tabela.dropna()
+    tabela["Salário Anual (R$)"] = pd.to_numeric(tabela["Salário Anual (R$)"], "coerce")
     return tabela
-
--- Analisis de tabela/main.py
+++ Analisis de tabela/main.py
@@ -1,19 +1,38 @@
 from get_tabela_data import get_tabela
-import plotly.express as px
 import matplotlib.pyplot as plt
-if __name__ == '__main__':
 
-    salarios = get_tabela()[get_tabela().columns[3]]
-    edades = get_tabela()[get_tabela().columns[2]]
-        
-    plt.bar(x=salarios, 
-            height=edades,
-            width=get_tabela().__len__())
-    
-    plt.xlabel("Salario")
-    plt.ylabel("Edad")
+if __name__ == "__main__":
+    plt.subplot(131)
+    plt.bar(
+        x=get_tabela()["Salário Anual (R$)"],
+        height=get_tabela()["Nota (1-100)"],
+        width=100,
+    )
+    plt.subplot(132)
+    plt.bar(
+        x=get_tabela()["Experiência Trabalho"].sort_values(),
+        height=get_tabela()["Nota (1-100)"].sort_values(),
+    )
     plt.show()
 
+    data = dict()
+    promedio = float()
 
-pass    
-  
+    for x in range(100):
+        key = get_tabela()["Profissão"].values[x]
+        value = get_tabela()["Nota (1-100)"].values[x]
+        data.setdefault(key, []).append(value)
+        pass
+
+    for key in list(data.keys()):
+        notes = data[key]
+        for aux in notes:
+            promedio = promedio + aux
+            pass
+        promedio = promedio / len(notes)
+        data[key] = promedio
+        pass
+
+    plt.bar(x=list(data.keys()), height=list(data.values()))
+    plt.show()
+pass
